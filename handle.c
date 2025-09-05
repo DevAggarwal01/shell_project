a62229cfa87152d86ba2270cf6196acf01027eb6
Dev Aggarwal <dev4dev@cs.utexas.edu>
2025-09-05 16:42:24 -0500

    part 1 done

--- handle.c
+++ handle.c
@@ -8,6 +8,14 @@
 #include "util.h"
 
 
+void handler(int sig) {
+    ssize_t bytes;
+    const int STDOUT = 1;
+    bytes = write(STDOUT, "Nice try.\n", 10);
+    if(bytes != 10)
+      exit(-999);
+}
+
 /*
  * First, print out the process ID of this process.
  *
@@ -20,8 +28,16 @@
 int main(int argc, char **argv)
 {
   printf("%d", (int) getpid());
-  
+  // ctrl + c is #2 signal
+  signal_action(2, handler);
+
+  while(1) {
+    printf("Still here\n");
+    nanosleep((const struct timespec[]){{1, 0}}, NULL);
+  }
   return 0;
 }
+
+
 
 
